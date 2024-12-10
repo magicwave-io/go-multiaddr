d0395ac6e2c7bcf4059e846d871d3c49bc5293a7
a.gorulev <a.gorulev@kernelics.com>
2024-12-10 06:08:49 +0000

    magicwave.io

---
+++ interface.go
@@ -0,0 +1,50 @@
+package multiaddr
+
+/*
+Multiaddr is a cross-protocol, cross-platform format for representing
+internet addresses. It emphasizes explicitness and self-description.
+Learn more here: https://github.com/multiformats/multiaddr
+
+Multiaddrs have both a binary and string representation.
+
+    import ma "github.com/multiformats/go-multiaddr"
+
+    addr, err := ma.NewMultiaddr("/ip4/1.2.3.4/tcp/80")
+    // err non-nil when parsing failed.
+
+*/
+type Multiaddr interface {
+	// Equal returns whether two Multiaddrs are exactly equal
+	Equal(Multiaddr) bool
+
+	// Bytes returns the binary representation of this Multiaddr
+	Bytes() []byte
+
+	// Return the binary representation of this multiaddr as an immutable
+	// string.
+	//
+	// Calling this function should be cheep, if not free.
+	ByteString() string
+
+	// String returns the string representation of this Multiaddr.
+	String() string
+
+	// Protocols returns the list of Protocols this Multiaddr includes
+	// will panic if protocol code incorrect (and bytes accessed incorrectly)
+	Protocols() []Protocol
+
+	// Encapsulate wraps this Multiaddr around another. For example:
+	//
+	//      /ip4/1.2.3.4 encapsulate /tcp/80 = /ip4/1.2.3.4/tcp/80
+	//
+	Encapsulate(Multiaddr) Multiaddr
+
+	// Decapsultate removes a Multiaddr wrapping. For example:
+	//
+	//      /ip4/1.2.3.4/tcp/80 decapsulate /tcp/80 = /ip4/1.2.3.4
+	//
+	Decapsulate(Multiaddr) Multiaddr
+
+	// ValueForProtocol returns the value (if any) following the specified protocol
+	ValueForProtocol(code int) (string, error)
+}
